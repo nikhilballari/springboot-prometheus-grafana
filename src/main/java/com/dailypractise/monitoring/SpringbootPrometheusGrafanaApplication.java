eee7212a9c68f054952528f90b70a368da83f6de
nikhilballari <nikhil.ballari@gmail.com>
2024-04-26 11:03:00 +0100

    springboot project for prometheus and grafana dashboard monitoring

---
+++ src/main/java/com/dailypractise/monitoring/SpringbootPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.dailypractise.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailypractise/monitoring/SpringbootPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailypractise.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
